294e820f781bd3b43f674d251eafc8cc3ad6ed71
Javi96 <josejaco@ucm.es>
2018-06-19 17:10:00 +0200

    Añadida pantalla de configuracion

---
+++ test2/app/src/main/java/com/pad/test2/myapplication/ConfigurationActivity.java
@@ -0,0 +1,14 @@
+package com.pad.test2.myapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ConfigurationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+
+}
