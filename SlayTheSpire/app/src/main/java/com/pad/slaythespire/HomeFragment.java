f45e4bd92ca6f68e40f6c00b8a8ecace2d652cb7
Javi <javi.tejada96@gmail.com>
2018-08-21 21:09:28 +0200

    Pantalla principal con cosas

--- SlayTheSpire/app/src/main/java/com/pad/slaythespire/HomeFragment.java
+++ SlayTheSpire/app/src/main/java/com/pad/slaythespire/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.pad.slaythespire;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -23,6 +27,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        /*View view = inflater.inflate(R.layout.fragment_home, container, false).getRootView();
+        TextView textView = view.findViewById(R.id.player_info);
+        Typeface typeface = Typeface.createFromAsset(this.getActivity().getAssets(),"fonts/Kreon-Bold.rtf");
+        textView.setTypeface(typeface);*/
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
